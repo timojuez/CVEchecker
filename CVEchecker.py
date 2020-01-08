482950ddd853757e3aba0cb2a42e9ba66dc2c29e
Timo Richter <timo.richter@t-systems.com>
2020-01-08 15:03:21 +0100

    switch to sqlite

--- CVEchecker.py
+++ CVEchecker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import json, sys
-import pandas as pd
+import pugsql
 from pprint import pprint
 import argparse
 import os
@@ -10,17 +10,22 @@ import datetime
 import requests
 
 
+cve_db = pugsql.module("queries/")
+cve_db.connect("sqlite:///cve_db.db")
+
+        
 def load_cve_whitelist(f):
     cves = []
     with open(f, encoding='utf-8') as p_file:
         for line in p_file:
             cves.append(line.replace("\n", "").replace("\r", ""))
     return cves    
-  
+
 
 def sanitize_version(version):
     # splitting app version examples:
     # 2.0.4~r204
+    if version[0]=="v": version = version[1:]
     version = version.split("~")[0]
     # 2.0.26-1ubuntu2
     version = version.split("-")[0]
@@ -37,16 +42,15 @@ def create_packages_file():
     for app in apps:
         if ("/") in app:
             name=app.split("/")[0]
-            version = sanitize_version(app.split()[1])
+            version = app.split()[1]
             p_file.write("{0} {1}\n".format(name,version))
     p_file.close
     
     
-def get_installed_packages(f):
-    p = []
+def load_packages(f):
     with open(f, encoding='utf-8') as p_file:
-        for line in p_file:
-            p.append(line)
+        p=[line.split(" ",1) for line in p_file]
+    p = [(name,sanitize_version(version)) for name,version in p]
     return p
 
     
@@ -58,11 +62,13 @@ class CVE_Parser(object):
             cve_db_paths = args.cve_dbs.split(",")
         else:
             cve_db_paths = self._download_cve_dbs()
+        cve_db.create_source()
+        cve_db.create_cve()
+        cve_db.create_product()
         cve_dbs = CVE_Parser._convert_cve_dbs(cve_db_paths)
         print ("\n[*] {0} CVE databases loaded:".format(len(cve_db_paths)))
         for db_path in cve_db_paths:
             print ("[*] {0}".format(db_path))
-        cve_dbs.to_pickle("csv_db.pkl")
         return cve_dbs
 
     @classmethod
@@ -70,6 +76,7 @@ class CVE_Parser(object):
         current_year=datetime.datetime.now().year
         years=range(2002, current_year + 1)
         print ("\n[*] download CVEs from {0}-{1}".format("2002", current_year))
+        years=[] # TODO: skipping
         for year in years:
             zip_file_url = "https://nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-{0}.json.zip".format(year)
             print ("[*] download and extract {0}".format(zip_file_url))
@@ -89,17 +96,17 @@ class CVE_Parser(object):
     @classmethod
     def _convert_cve_dbs(self,cve_db_paths):
         sys.stderr.write("[*] Converting database")
-        cve_dbs = []
         for cve_db_path in cve_db_paths:
-            with open(cve_db_path, encoding='utf-8') as path:
-                cve_dbs.extend(self._parseDB(json.load(path)))
-            sys.stderr.write(".")
+            with open(cve_db_path, encoding='utf-8') as fp:
+                self._parseDB(cve_db_path,json.load(fp))
+            sys.stderr.write(".");sys.stderr.flush()
         sys.stderr.write("\n")
-        return pd.DataFrame(cve_dbs)
+        #cve_db.session.commit()
         
     @classmethod
     def load_cve_db(self):
-        return pd.read_pickle("csv_db.pkl")
+        #return pd.read_pickle("csv_db.pkl")
+        pass
 
     @classmethod
     def _parseImpact(self,cve):
@@ -107,15 +114,39 @@ class CVE_Parser(object):
             base_metric='cvssV3'
             impact_score=cve['impact']['baseMetricV3']['cvssV3']['baseScore']
             impact_severity=cve['impact']['baseMetricV3']['cvssV3']['baseSeverity']
-        else:
+        elif 'baseMetricV2' in cve['impact']:
             base_metric='cvssV2'
             impact_score=cve['impact']['baseMetricV2']['cvssV2']['baseScore']
             impact_severity=cve['impact']['baseMetricV2']['severity']  
+        else: raise
         return base_metric,impact_score,impact_severity
     
     @classmethod
-    def _parseDB(self,cve_db):
-        return [dict(
+    def _parseDB(self,path,cve_db_):
+        source_id = cve_db.insert_source(filename=path,added_on=str(datetime.datetime.now()))
+        
+        for cve in cve_db_["CVE_Items"]:
+            if len(cve["cve"]["affects"]["vendor"]["vendor_data"]) == 0: continue
+            base_metric,impact_score,impact_severity = self._parseImpact(cve)
+            cve_id = cve_db.insert_cve(
+                source=source_id,
+                cve_id=cve['cve']['CVE_data_meta']['ID'],
+                cve_description=cve['cve']['description']['description_data'][0]['value'],
+                base_metric=base_metric,
+                impact_score=impact_score,
+                impact_severity=impact_severity,
+            )
+            cve_db.insert_product(*tuple([dict(
+                            cve=cve_id,
+                            product_name=product_data['product_name'],
+                            product_version=version_data['version_value'],
+                )
+                for vendor in cve['cve']['affects']['vendor']['vendor_data']
+                for product_data in vendor['product']['product_data']
+                for version_data in product_data['version']['version_data']
+            ]))
+        """
+        return [db.insert(cve_db.cve).values(
                 product_name=product_data['product_name'],
                 product_version=version_data['version_value'],
                 cve_id=cve['cve']['CVE_data_meta']['ID'],
@@ -124,8 +155,9 @@ class CVE_Parser(object):
                 impact_score=impact_score,
                 impact_severity=impact_severity,
             )
-            for cve in cve_db["CVE_Items"] for vendor in cve['cve']['affects']['vendor']['vendor_data'] for product_data in vendor['product']['product_data']
+            for cve in cve_db_["CVE_Items"] for vendor in cve['cve']['affects']['vendor']['vendor_data'] for product_data in vendor['product']['product_data']
             for version_data in product_data['version']['version_data'] for base_metric,impact_score,impact_severity in [self._parseImpact(cve)] ]
+        """
 
 
 def check_packages(packages, cve_dbs, whitelist):
@@ -133,8 +165,23 @@ def check_packages(packages, cve_dbs, whitelist):
     @packages: [(name,version)]
     @cve_dbs: see CVE_Parser.parseDB
     """
-    import ipdb;ipdb.set_trace()
-
+    try:
+        cve_db._execute("DROP TABLE packages")
+    except Exception as e: pass
+    
+    with cve_db.transaction() as t:
+        try:
+            cve_db.create_packages()
+            for name,version in packages: 
+                cve_db.insert_package(product_name=name,product_version=version)
+            rows = cve_db.get_cves()
+        finally: 
+            t.rollback()
+    for d in rows:
+        print(("%(product_name)s %(product_version)s\t"
+            "%(cve_id)s (%(base_metric)s: %(impact_score)s, %(impact_severity)s)")%d)
+    
+    
 def check_package (package, cve_dbs, whitelist):
     """
     packages: (name, version)
@@ -174,24 +221,21 @@ def check_package (package, cve_dbs, whitelist):
                                 
 
 def check():
+    cve_whitelist=[]
     if args.csv:
         csv_file = open(args.csv, 'w')
-    packages = get_installed_packages(packages_file)
+    packages = load_packages(packages_file)
     print ("\n[*] {0} packages to check:".format(len(packages)))
-    for p in packages:
-        name = p.split()[0]
-        version = p.split()[1]
+    for name,version in packages:
         print ("[*] {0} {1}".format(name,version))
     if args.whitelist_file:
         cve_whitelist=load_cve_whitelist(args.whitelist_file)
-    print ("\n[*] {0} CVEs whitelisted:".format(len(cve_whitelist)))
-    for cve_id in cve_whitelist:
-        print ("[*] {0}".format(cve_id)) 
+        print ("\n[*] {0} CVEs whitelisted:".format(len(cve_whitelist)))
+        for cve_id in cve_whitelist:
+            print ("[*] {0}".format(cve_id)) 
     cve_dbs=CVE_Parser.load_cve_db()
-    check(packages, cve_dbs, cve_whitelist)
     if args.csv:
         csv_file.write("Name;Matching Name;Version;CVE;Metric;Score;Severity;Description\n")
-    packages = [(name,version) for package in packages for name,version in [package.split(" ",1)]]
     check_packages(packages, cve_dbs, cve_whitelist)
     if args.csv:
         csv_file.close   
@@ -216,7 +260,7 @@ def parseArgs():
 # defaults
 packages_file=""
 cve_db_paths=[]
-cve_whitelist=[]
+
 
 if __name__ == '__main__':
     parseArgs()
@@ -230,6 +274,6 @@ if __name__ == '__main__':
         packages_file = args.packages_file
     else:
         packages_file = './packages.txt'
-        
+
     if not args.no_check: check()
- 
+
