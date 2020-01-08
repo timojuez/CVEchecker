948b1bbddde357224e4599c5337c0a292de4bab9
Timo Richter <timo.richter@t-systems.com>
2020-01-08 17:01:49 +0100

    object orientation

--- CVEchecker.py
+++ CVEchecker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import json, sys
+import json, sys, csv
 import pugsql
 from pprint import pprint
 import argparse
@@ -14,69 +14,66 @@ cve_db = pugsql.module("queries/")
 cve_db.connect("sqlite:///cve_db.db")
 
         
-def load_cve_whitelist(f):
-    cves = []
-    with open(f, encoding='utf-8') as p_file:
-        for line in p_file:
-            cves.append(line.replace("\n", "").replace("\r", ""))
-    return cves    
-
-
-def sanitize_version(version):
-    # splitting app version examples:
-    # 2.0.4~r204
-    if version[0]=="v": version = version[1:]
-    version = version.split("~")[0]
-    # 2.0.26-1ubuntu2
-    version = version.split("-")[0]
-    # 3.113+nmu3ubuntu4
-    version = version.split("+")[0]
-    # 4:15.1
-    if ":" in version:
-        version = version.split(":")[1]
-    return version
-    
-def create_packages_file():
-    apps=os.popen("apt list --installed").readlines()
-    p_file = open("./packages.txt", 'w')
-    for app in apps:
-        if ("/") in app:
-            name=app.split("/")[0]
-            version = app.split()[1]
-            p_file.write("{0} {1}\n".format(name,version))
-    p_file.close
-    
-    
-def load_packages(f):
-    with open(f, encoding='utf-8') as p_file:
-        p=[line.strip().split(" ",1) for line in p_file]
-    p = [(name,sanitize_version(version)) for name,version in p]
-    return p
-
-    
-class CVE_Parser(object):
-
+class PackageLoader(object):
+    """ Reads the list of the customer's packages in use """
+  
     @classmethod
-    def install_cve_from_web(self):
-        if args.cve_dbs:
-            cve_db_paths = args.cve_dbs.split(",")
+    def create_packages_file(self):
+        apps=os.popen("apt list --installed").readlines()
+        p_file = open("./packages.txt", 'w')
+        
+        for app in apps:
+            if ("/") in app:
+                name=app.split("/")[0]
+                version = app.split()[1]
+                p_file.write("{0} {1}\n".format(name,version))
+        p_file.close
+        
+    def __init__(self,f):
+        with open(f, encoding='utf-8') as p_file:
+            p=[line.strip().split(" ",1) for line in p_file]
+        p = [(name,self._sanitize_version(version)) for name,version in p]
+        print("\n[*] {0} packages to check:".format(len(p)))
+        for name,version in p:
+            print ("[*] {0} {1}".format(name,version))
+        self.packages = p
+
+    def _sanitize_version(self,version):
+        # splitting app version examples:
+        # 2.0.4~r204
+        if version[0]=="v": version = version[1:]
+        version = version.split("~")[0]
+        # 2.0.26-1ubuntu2
+        version = version.split("-")[0]
+        # 3.113+nmu3ubuntu4
+        version = version.split("+")[0]
+        # 4:15.1
+        if ":" in version:
+            version = version.split(":")[1]
+        return version
+
+    
+class CVE_DB_Installer(object):
+    """ Read CVE as JSON from the internet and create local database """
+
+    def __init__(self,json_db_paths=None):
+        if json_db_paths:
+            cve_db_paths = json_db_paths.split(",")
         else:
             cve_db_paths = self._download_cve_dbs()
         cve_db.create_source()
         cve_db.create_cve()
         cve_db.create_product()
-        cve_dbs = CVE_Parser._convert_cve_dbs(cve_db_paths)
+        cve_dbs = self._convert_cve_dbs(cve_db_paths)
         print ("\n[*] {0} CVE databases loaded:".format(len(cve_db_paths)))
         for db_path in cve_db_paths:
             print ("[*] {0}".format(db_path))
-        return cve_dbs
 
-    @classmethod
     def _download_cve_dbs(self):
         current_year=datetime.datetime.now().year
         years=range(2002, current_year + 1)
         print ("\n[*] download CVEs from {0}-{1}".format("2002", current_year))
-        years=[] # TODO: skipping
+        #years=[] # TODO: skipping
         for year in years:
             zip_file_url = "https://nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-{0}.json.zip".format(year)
             print ("[*] download and extract {0}".format(zip_file_url))
@@ -93,17 +90,14 @@ class CVE_Parser(object):
                 cve_db_paths.append(f)
         return cve_db_paths
 
-    @classmethod
     def _convert_cve_dbs(self,cve_db_paths):
         sys.stderr.write("[*] Converting database")
         for cve_db_path in cve_db_paths:
             with open(cve_db_path, encoding='utf-8') as fp:
-                self._parseDB(cve_db_path,json.load(fp))
+                self._parseJSON(cve_db_path,json.load(fp))
             sys.stderr.write(".");sys.stderr.flush()
         sys.stderr.write("\n")
-        #cve_db.session.commit()
         
-    @classmethod
     def _parseImpact(self,cve):
         if 'baseMetricV3' in cve['impact']:
             base_metric='cvssV3'
@@ -116,11 +110,10 @@ class CVE_Parser(object):
         else: raise
         return base_metric,impact_score,impact_severity
     
-    @classmethod
-    def _parseDB(self,path,cve_db_):
+    def _parseJSON(self,path,json_d):
         source_id = cve_db.insert_source(filename=path,added_on=str(datetime.datetime.now()))
         
-        for cve in cve_db_["CVE_Items"]:
+        for cve in json_d["CVE_Items"]:
             if len(cve["cve"]["affects"]["vendor"]["vendor_data"]) == 0: continue
             base_metric,impact_score,impact_severity = self._parseImpact(cve)
             cve_id = cve_db.insert_cve(
@@ -140,96 +133,82 @@ class CVE_Parser(object):
                 for product_data in vendor['product']['product_data']
                 for version_data in product_data['version']['version_data']
             ]))
-        """
-        return [db.insert(cve_db.cve).values(
-                product_name=product_data['product_name'],
-                product_version=version_data['version_value'],
-                cve_id=cve['cve']['CVE_data_meta']['ID'],
-                cve_description=cve['cve']['description']['description_data'][0]['value'],
-                base_metric=base_metric,
-                impact_score=impact_score,
-                impact_severity=impact_severity,
-            )
-            for cve in cve_db_["CVE_Items"] for vendor in cve['cve']['affects']['vendor']['vendor_data'] for product_data in vendor['product']['product_data']
-            for version_data in product_data['version']['version_data'] for base_metric,impact_score,impact_severity in [self._parseImpact(cve)] ]
-        """
 
 
-def check_packages(packages, whitelist):
-    """
-    @packages: [(name,version)]
-    """
-    try:
-        cve_db._execute("DROP TABLE packages")
-    except Exception as e: pass
+class CVE_Finder(object):
     
-    with cve_db.transaction() as t:
+    def __init__(self,packages, whitelist):
+        """
+        @packages: [(name,version)]
+        """
         try:
-            cve_db.create_packages()
-            for name,version in packages: 
-                cve_db.insert_package(product_name=name,product_version=version)
-            rows = cve_db.get_cves()
-        finally: 
-            t.rollback()
-    for d in rows:
-        print(("%(product_name)s %(product_version)s\t"
-            "%(cve_id)s\t%(base_metric)s: %(impact_score)s, %(impact_severity)s")%d)
-    
-    
-
-def check():
-    cve_whitelist=[]
-    if args.csv:
-        csv_file = open(args.csv, 'w')
-    packages = load_packages(packages_file)
-    print ("\n[*] {0} packages to check:".format(len(packages)))
-    for name,version in packages:
-        print ("[*] {0} {1}".format(name,version))
-    if args.whitelist_file:
-        cve_whitelist=load_cve_whitelist(args.whitelist_file)
-        print ("\n[*] {0} CVEs whitelisted:".format(len(cve_whitelist)))
-        for cve_id in cve_whitelist:
-            print ("[*] {0}".format(cve_id)) 
-    if args.csv:
-        csv_file.write("Name;Matching Name;Version;CVE;Metric;Score;Severity;Description\n")
-    check_packages(packages, cve_whitelist)
-    if args.csv:
-        csv_file.close   
+            cve_db._execute("DROP TABLE packages")
+        except Exception as e: pass
         
-def parseArgs():
-    global args
-    parser = argparse.ArgumentParser(description="This little tool helps you to identify vulnerable software packages, by looking them up in the CVE (Common Vulnerabilities and Exposure) databases from the NVD. CVEchecker is designed to work offline. It gets fed with two files, the package list file and a cve database file(s). These can be obtained manually or by using the paramaters --download-cve-dbs and --create-packages-file.")
-
-    parser.add_argument('--download-cve-dbs', action="store_true", help='Download and extract all CVE databases since 2002 from https://nvd.nist.gov/vuln/data-feeds#JSON_FEED). More than 1 GB of free harddrive space is needed.')
-    parser.add_argument('--create-packages-file', action="store_true", help='Create a list of installed packages and corresponding versions. Just works for packages installed with APT.')
-    parser.add_argument('--packages-file', help='A whitespace seperated list with software name and version. If parameter is not set, the file ./packages.txt will be loaded by default.')
-    parser.add_argument('--cve-dbs', help='Path to CVE database file(s). Multiple paths must be seperated by a comma. The json content must follow the NVD JSON 0.1 beta Schema (https://nvd.nist.gov/vuln/data-feeds#JSON_FEED). If parameter is not set, all files with the name \"nvdcve-1.0-YYYY.json\" will be loaded by default.')
-    parser.add_argument('--whitelist-file', help="A list of CVEs (format: 'CVE-2018-10546') which won't show up in the result. Can be used to exclude false-positives.")
-    parser.add_argument('--no-check', action="store_true", help='Use it together with --download-cve-db or --create-packages-file to skip the cve checking process afterwards.')
-    parser.add_argument('--csv', help='File name where results shall be stored.')
-
-    args = parser.parse_args()
+        with cve_db.transaction() as t:
+            try:
+                cve_db.create_packages()
+                for name,version in packages: 
+                    cve_db.insert_package(product_name=name,product_version=version)
+            finally: 
+                t.rollback()
+            self.cves = list(cve_db.get_cves())
+        for d in self.cves:
+            print(("%(product_name)s %(product_version)s\t"
+                "%(cve_id)s\t%(base_metric)s: %(impact_score)s, %(impact_severity)s")%d)
 
 
+class Main(object):
 
+    def __init__(self):
+        self.parseArgs()
+        if self.args.download_cve_dbs:
+            CVE_DB_Installer(self.args.cve_dbs)
 
-# defaults
-packages_file=""
-cve_db_paths=[]
+        if self.args.create_packages_file:
+            PackageLoader.create_packages_file()
+
+        if not self.args.no_check: self.check()
+
+    def check(self):
+        if self.args.packages_file:
+            packages_file = self.args.packages_file
+        else:
+            packages_file = './packages.txt'
+        packages = PackageLoader(package_file).packages
+
+        cve_whitelist=self.load_cve_whitelist(args.whitelist_file) if args.whitelist_file else []
+        finder = CVE_Finder(packages, cve_whitelist)
+        if self.args.csv:
+            with open(self.args.csv, 'w') as fp:
+                writer=csv.DictWriter(fp, fieldnames=finder.cves.keys())
+                writer.writeheader()
+                for data in finder.cves: writer.writerow(data)
+
+    def load_cve_whitelist(self,f):
+        cves = []
+        with open(f, encoding='utf-8') as p_file:
+            for line in p_file:
+                cves.append(line.replace("\n", "").replace("\r", ""))
+        print ("\n[*] {0} CVEs whitelisted:".format(len(cves)))
+        for cve_id in cves:
+            print ("[*] {0}".format(cve_id)) 
+        return cves    
+
+    def parseArgs(self):
+        parser = argparse.ArgumentParser(description="This little tool helps you to identify vulnerable software packages, by looking them up in the CVE (Common Vulnerabilities and Exposure) databases from the NVD. CVEchecker is designed to work offline. It gets fed with two files, the package list file and a cve database file(s). These can be obtained manually or by using the paramaters --download-cve-dbs and --create-packages-file.")
+
+        parser.add_argument('--download-cve-dbs', action="store_true", help='Download and extract all CVE databases since 2002 from https://nvd.nist.gov/vuln/data-feeds#JSON_FEED). More than 1 GB of free harddrive space is needed.')
+        parser.add_argument('--create-packages-file', action="store_true", help='Create a list of installed packages and corresponding versions. Just works for packages installed with APT.')
+        parser.add_argument('--packages-file', help='A whitespace seperated list with software name and version. If parameter is not set, the file ./packages.txt will be loaded by default.')
+        parser.add_argument('--cve-dbs', default=None, help='Path to CVE database file(s). Multiple paths must be seperated by a comma. The json content must follow the NVD JSON 0.1 beta Schema (https://nvd.nist.gov/vuln/data-feeds#JSON_FEED). If parameter is not set, all files with the name \"nvdcve-1.0-YYYY.json\" will be loaded by default.')
+        parser.add_argument('--whitelist-file', help="A list of CVEs (format: 'CVE-2018-10546') which won't show up in the result. Can be used to exclude false-positives.")
+        parser.add_argument('--no-check', action="store_true", help='Use it together with --download-cve-db or --create-packages-file to skip the cve checking process afterwards.')
+        parser.add_argument('--csv', help='File name where results shall be stored.')
+
+        self.args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    parseArgs()
-    if args.download_cve_dbs:
-        CVE_Parser.install_cve_from_web()
-
-    if args.create_packages_file:
-        create_packages_file()
-
-    if args.packages_file:
-        packages_file = args.packages_file
-    else:
-        packages_file = './packages.txt'
-
-    if not args.no_check: check()
+    Main()()
 
