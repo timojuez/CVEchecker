18e2f7bd0fb1241650213fcf790da0fb8c8a4177
Timo Richter <timo.richter@t-systems.com>
2020-01-09 13:25:59 +0100

    blacklist function

--- queries/get_cves.sql
+++ queries/get_cves.sql
@@ -6,5 +6,6 @@ ON lower(product.product_name) = lower(packages.product_name)
 AND product.product_version = packages.product_version -- TODO
 LEFT JOIN cve
 ON cve.rowid = product.cve
+WHERE cve_id NOT IN :blacklist
 ORDER BY impact_score DESC, lower(product.product_name) ASC;
 
