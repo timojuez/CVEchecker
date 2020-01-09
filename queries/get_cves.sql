6ed7074b94926ccba53a55e9be8f986a7c80aa7b
Timo Richter <timo.richter@t-systems.com>
2020-01-09 12:56:37 +0100

    ordering case insensitive

--- queries/get_cves.sql
+++ queries/get_cves.sql
@@ -2,9 +2,9 @@
 SELECT packages.*, cve.*
 FROM packages
 LEFT JOIN product
-ON product.product_name = packages.product_name
+ON lower(product.product_name) = lower(packages.product_name)
 AND product.product_version = packages.product_version -- TODO
 LEFT JOIN cve
 ON cve.rowid = product.cve
-ORDER BY impact_score DESC;
+ORDER BY impact_score DESC, lower(product.product_name) ASC;
 
