114c64b3ac31d35e5f4b2609720127cd9be15838
Timo Richter <timo.richter@t-systems.com>
2020-02-07 12:02:07 +0100

    minor fixes

--- find_availability_cves.py
+++ find_availability_cves.py
@@ -5,10 +5,10 @@ from cvss import CVSS3
 from db import *
 
 
-sys.stderr.write("Creating a CVE list of all CVEs from database where integrity=none and confidentiality=none and output to stdout.")
+sys.stderr.write("Creating a CVE list of all CVEs from database where integrity=none and confidentiality=none and output to stdout.\n")
 
 metrics = [(e["cve_id"],CVSS3(e["vector"]).metrics) 
     for e in cve_db._execute("SELECT * FROM cve").fetchall()]
-cves = [id_ for id_, metric in metrics if metric["I"] == "N" and metric["C"] == "N"]]
+cves = [id_ for id_, metric in metrics if metric["I"] == "N" and metric["C"] == "N"]
 for cve in cves: print(cve)
 
