e5a0129d6346dc0dd92f522fd6b9ebdc5c454d34
Timo Richter <timo.richter@t-systems.com>
2020-05-14 16:01:48 +0200

    counting fix

--- count_csv_rows.py
+++ count_csv_rows.py
@@ -7,13 +7,15 @@ from db import *
 class Main(object):
 
     def __init__(self):
-        parser = argparse.ArgumentParser(description="Count number of rows in one or more CSV files")
+        parser = argparse.ArgumentParser(description="Count amount of CVEs in one or more CSV files")
         parser.add_argument('csv', nargs="+", metavar="CSV", help="Read and modify this file")
         self.args = parser.parse_args()
 
     def readcsv(self,path):
         with open(path,"r") as fp:
-            return len(list(csv.DictReader(fp)))
+            dr = csv.DictReader(fp)
+            rows = [1 for r in dr if r["cve_id"]]
+            return len(rows)
 
     def __call__(self):
         amount = sum([self.readcsv(f) for f in self.args.csv])
