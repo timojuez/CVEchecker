2220b50f928f3ac64eb15b7f591ee3ed769d6e97
Timo Richter <timo.richter@t-systems.com>
2020-05-14 18:56:43 +0200

    setup include query files

--- cvec/db.py
+++ cvec/db.py
@@ -1,5 +1,9 @@
 import pugsql
+from importlib import resources
 
-cve_db = pugsql.module("queries/")
+_generator = resources.path("cvec","queries")
+_path = _generator.__enter__()
+
+cve_db = pugsql.module(_path)
 cve_db.connect("sqlite:///cve_db.db")
 
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools, os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cvec",
+    version="0.0.1",
+    author="Timo Richter",
+    author_email="",
+    description="CVE checker - Find CVEs that affect given program(s)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/timojuez/CVEchecker",
+    install_requires=["cvss","cvsslib","zipp","requests","pugsql"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    scripts=["bin/%s"%f for f in os.listdir("bin")],
+    python_requires='>=3.6',
+    include_package_data=True,
+)
+
