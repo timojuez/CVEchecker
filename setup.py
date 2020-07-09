9f0f26296c9e05f9bc5cf65bbbc80154f4c9c2c4
Timo Richter <timo.richter@t-systems.com>
2020-07-09 08:40:23 +0200

    bugfix: loading DB version 1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cvec",
-    version="0.2.1",
+    version="0.2.2",
     author="Timo Richter",
     author_email="",
     description="CVE checker - Find CVEs that affect given program(s)",
